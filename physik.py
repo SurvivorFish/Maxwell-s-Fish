c63521ffde125b11e873aef2ecd9753b72dbf8bc
bunt <froschimwind85@gmail.com>
2025-06-12 00:25:30 +0300

    аоаоаоаооа

--- physik.py
+++ physik.py
@@ -7,11 +7,12 @@ gwkeit = np.random.randn(N, 2) * 300
 def tritt():
     global lage, gwkeit, karton_breite, karton_lange
     lage += dt * gwkeit
-    gw = np.linalg.norm(gwkeit[i]) 
     print("lage[0]:", lage[0])
     for i in range(N):
+        gw = np.linalg.norm(gwkeit[i])
         x, y = lage[i]
-        v_x, v_y = gwkeit[i]
+        v_x = gwkeit[i,0]
+        v_y = gwkeit[i,1]
     if y <= 0 or y >= karton_breite:
         gwkeit[i, 1] *= -1
     if x <= 0 or x >= karton_lange:
