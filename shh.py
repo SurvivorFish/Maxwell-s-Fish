ce92a79e1518f98d4bda16e55ecb97a1164d585c
bunt <froschimwind85@gmail.com>
2025-06-14 18:51:02 +0300

    ei

--- shh.py
+++ shh.py
@@ -10,27 +10,27 @@ grenze_v = 100
 
 # начальные координаты и скорости
 lage = np.random.rand(N, 2) * [karton_lange, karton_breite]
-gwekit = np.random.randn(N, 2) * 20
+gwkeit = np.random.randn(N, 2) * 20
 
 # функция обновления
 def tritt():
-    global lage, gwekit
+    global lage, gwkeit
     for i in range(N):
         x, y = lage[i]
-        vx, vy = gwekit[i]
+        vx, vy = gwkeit[i]
 
         # отражения от границ
         if x <= 0 or x >= karton_lange:
-            gwekit[i, 0] *= -1
+            gwkeit[i, 0] *= -1
         if y <= 0 or y >= karton_breite:
-            gwekit[i, 1] *= -1
+            gwkeit[i, 1] *= -1
         if abs(x - karton_lange / 2) < 0.05:
-            v = np.linalg.norm(gwekit[i])
+            v = np.linalg.norm(gwkeit[i])
             if x < karton_lange / 2 and vx > 0 and v > grenze_v:
-                gwekit[i, 0] *= -1
+                gwkeit[i, 0] *= -1
             elif x > karton_lange / 2 and vx < 0 and v < grenze_v:
-                gwekit[i, 0] *= -1
-        lage[i] += gwekit[i] * dt
+                gwkeit[i, 0] *= -1
+        lage[i] += gwkeit[i] * dt
 
 # отрисовка
 plt.ion()
